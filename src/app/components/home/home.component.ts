6d7219e7db35316e640be5bd401f2d1d18187e6c
lucasmagalhaes33 <lucas.magalhaes911@gmail.com>
2024-08-20 08:42:58 -0300

    feat: adicionando home do projeto.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
+ import {HeaderComponent} from "../header/header.component";
+ import {RouterOutlet} from "@angular/router";
+ import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    RouterOutlet,
+    NgOptimizedImage
+  ],
+  providers: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
